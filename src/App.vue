5ebf52fe5b42660f20be72979b202b0ca7c3380a
Fatih Çakmak <cakmakfatih@outlook.com.tr>
2019-01-03 01:46:26 +0300

    frontend complete

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <form @submit="search">
+      <h1 class="title is-2">
+        CryptoSearcher
+      </h1>
+      <h2 class="subtitle">
+        Search any crypto coin by it's symbol to retrieve up-to-date data.
+      </h2>
+      <div class="level-item has-text-centered">
+        <div class="field has-addons">
+          <div class="control">
+            <input class="input is-primary is-medium" type="text" placeholder="Search" v-model="query">
+          </div>
+          <div class="control">
+            <button class="button is-primary is-medium" :disabled="loading">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+    <div class="loader" style="height: 50px; width: 50px;" v-if="loading"></div>
+    <div v-if="result == null && !loading">
+      <span class="subtitle">
+        Couldn't find anything similiar to your search.
+      </span>
+    </div>
+    <app-currency v-bind:currency="result" v-if="!loading && result"></app-currency>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Vue, Component } from 'vue-property-decorator';
+import CurrencyComponent from './components/CurrencyComponent.vue';
+import { Currency } from './models/currency.model';
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    apiEndpoint: string;
+    result?: Currency;
+    loading: boolean;
+    query: string;
+    lastSearch: string;
+    fetchData: Function;
+  }
+}
 
 @Component({
   components: {
-    HelloWorld,
+    "app-currency": CurrencyComponent
   },
+  data() {
+    return {
+      apiEndpoint: "http://127.0.0.1:3000/coin?search=",
+      result: null,
+      loading: null,
+      query: ""
+    };
+  },
+  methods: {
+    async fetchData(query: string) {
+      if(!(this.result && this.result.status === "success" && query.toUpperCase() === this.result.symbol.toUpperCase()) && query !== this.lastSearch) {
+        this.lastSearch = query;
+        this.loading = true;
+        let res = await fetch(`${this.apiEndpoint}${query}`).then(re => re.json());
+        if(res.status !== "error") {
+          this.result = res;
+        } else {
+          this.result = undefined;
+        }
+        this.loading = false;
+      }
+    },
+    search(e: Event) {
+      e.preventDefault();
+      this.fetchData(this.query);
+    }
+  },
+  created() {
+    this.fetchData("btc");
+  }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  padding: 35px;
+  overflow-y: hidden;
+  flex-direction: column;
+  justify-content: center;
+}
+form {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.field {
+  margin-bottom: 20px;
+}
+.field > .control > input {
+  min-width: 40vw;
+}
+h2.subtitle {
+  text-align: center;
+}
+p.heading {
+  font-weight: bold;
 }
 </style>
---
+++ src/components/CurrencyComponent.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="container">
+    <div class="level">
+        <div class="level-item has-text-centered" v-if="currency.rank">
+            <div>
+                <p class="heading">Rank</p>
+                <p class="title">{{currency.rank}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.name">
+            <div>
+                <p class="heading">Name</p>
+                <p class="title">{{currency.name}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.symbol">
+            <div>
+                <p class="heading">Symbol</p>
+                <p class="title">{{currency.symbol}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.price_usd">
+            <div>
+                <p class="heading">Price (USD)</p>
+                <p class="title">{{currency.price_usd}}</p>
+            </div>
+    </div>
+    </div>
+    <div class="level">
+        <div class="level-item has-text-centered" v-if="currency.price_btc">
+            <div>
+                <p class="heading">Price (BTC)</p>
+                <p class="title">{{currency.price_btc}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency['24h_volume_usd']">
+            <div>
+                <p class="heading">24h Volume (USD)</p>
+                <p class="title">{{currency["24h_volume_usd"]}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.market_cap_usd">
+            <div>
+                <p class="heading">Market Cap (USD)</p>
+                <p class="title">{{currency.market_cap_usd}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.total_supply">
+            <div>
+                <p class="heading">Total Supply</p>
+                <p class="title">{{currency.total_supply}}</p>
+            </div>
+        </div>
+    </div>
+    <div class="level">
+        <div class="level-item has-text-centered" v-if="currency.max_supply">
+            <div>
+                <p class="heading">Max Supply</p>
+                <p class="title">{{currency.max_supply}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.percent_change_1h">
+            <div>
+            <p class="heading">Percent Change (1h)</p>
+            <p class="title">-{{currency.percent_change_1h}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.percent_change_24h">
+            <div>
+            <p class="heading">Percent Change (24h)</p>
+            <p class="title">{{currency.percent_change_24h}}</p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered" v-if="currency.percent_change_7d">
+            <div>
+            <p class="heading">Percent Change (7d)</p>
+            <p class="title">{{currency.percent_change_7d}}</p>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Currency } from './../models/currency.model';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CurrencyComponent extends Vue {
+  @Prop() private currency!: Currency;
+}
+</script>
+<style lang="scss" scoped>
+p.heading {
+    color: #616161;
+}
+</style>
